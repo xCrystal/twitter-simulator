2e8597f0eae6082ae72cbdbdc47b288b89033825
xCrystal <rgr.crystal@gmail.com>
2017-12-02 01:56:58 +0000

    Set up minimal twitter search functionality

--- twitter-simulator.js
+++ twitter-simulator.js
@@ -1,1 +1,12 @@
+import { Twitter } from 'twitter-node-client'
+import config from './twitter-config'
 
+const twitter = new Twitter(config);
+const error = (err) => {
+  console.log(err);
+};
+const success = (data) => {
+  console.log(data);
+};
+
+twitter.getTweet({ id: '1111111111' }, error, success);
