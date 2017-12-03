aaec8175bc08f0790e01edc8d87120f51c3a73ed
xCrystal <rgr.crystal@gmail.com>
2017-12-03 19:01:25 +0000

    Search with date and query

--- twitter-simulator.js
+++ twitter-simulator.js
@@ -1,12 +1,42 @@
 import { Twitter } from 'twitter-node-client'
 import config from './twitter-config'
+import words from './words.js'
 
-const twitter = new Twitter(config);
+const T = new Twitter(config);
 const error = (err) => {
-  console.log(err);
+  console.log('ERR ->', err);
 };
 const success = (data) => {
-  console.log(data);
+  data = JSON.parse(data);
+  for (let status of data.statuses) {
+    console.log(
+      "********************************************************************\n",
+      "ID:", status.id_str, "\n",
+      "Date:", status.created_at, "\n",
+      status.retweet_count, "RTs /", status.favorite_count, "favs", "\n",
+      "By:", status.user.name, "\n",
+      status.full_text,
+    )
+  }
 };
 
-twitter.getTweet({ id: '1111111111' }, error, success);
+const rnd = () => Math.random();
+const days = (n) => 1000 * 60 * 60 * 24 * n;
+let delta = Math.min(rnd(), rnd()) * days(9);
+let curTime = new Date().getTime();
+let word = '"' + words[
+  Math.floor(
+    Math.min(rnd(), rnd(), rnd(), rnd()) * words.length
+  )
+] + '\u0020"';
+console.log("THE WORD IS", word, ".");
+
+T.getSearch({
+  "q": word + " AND -filter:retweets",
+  "lang": "en",
+  "since": new Date(curTime - delta),
+  "until": new Date(curTime - delta + days(1)),
+  "result_type": "popular",
+  "tweet_mode": "extended",
+  "count": 8
+}, error, success);
