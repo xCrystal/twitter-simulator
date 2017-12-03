24598d589a0fe2b7eb3bf154d0eff6c2c8386267
xCrystal <rgr.crystal@gmail.com>
2017-12-03 22:23:37 +0000

    More helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+import s from 'underscore.string'
+
 export default {
 
   random: function(max, min = 0, bias = false) {
@@ -22,6 +24,31 @@ export default {
 
   randomDiscrete: function(max, min = 0, bias = false) {
     return Math.floor(this.random(max, min, bias));
+  },
+
+  timeInterval: function() {
+    const days = (n) => 1000 * 60 * 60 * 24 * n;
+    let delta = this.random(days(10), 0, -2);
+    let curTime = new Date().getTime();
+    return {
+      "since": new Date(curTime - delta),
+      "until": new Date(curTime - delta + days(3)),
+    };
+  },
+
+  clean: function(str) {
+    return s.clean(str);
+  },
+
+  deleteLink: function(str) {
+    return s.strLeft(str, "https://t.co/");
+  },
+
+  demention: function(str) {
+    return s.map(str, (x) => {
+      if(x === '@') x = '';
+      return x;
+    });
   },
 
   popRandom: function(list) {
--- twitter-simulator.js
+++ twitter-simulator.js
@@ -10,29 +10,30 @@ const error = (err) => {
 const success = (data) => {
   data = JSON.parse(data);
   for (let status of data.statuses) {
+    let text = H.clean(status.full_text);
+    text = H.deleteLink(text);
+    text = H.demention(text);
     console.log(
       "********************************************************************\n",
       "ID:", status.id_str, "\n",
       "Date:", status.created_at, "\n",
       status.retweet_count, "RTs /", status.favorite_count, "favs", "\n",
       "By:", status.user.name, "\n",
-      status.full_text,
+      text,
     )
   }
 };
 
-const rnd = () => Math.random();
-const days = (n) => 1000 * 60 * 60 * 24 * n;
-let delta = H.random(days(10), 0, -2);
-let curTime = new Date().getTime();
+let interval = H.timeInterval();
+console.log(interval.since, interval.until)
 let word = '"' + words[H.randomDiscrete(words.length, 0, -4)] + '\u0020"';
 console.log("THE WORD IS", word, ".");
 
 T.getSearch({
   "q": word + " AND -filter:retweets",
   "lang": "en",
-  "since": new Date(curTime - delta),
-  "until": new Date(curTime - delta + days(3)),
+  "since": interval.since,
+  "until": interval.until,
   "result_type": "popular",
   "tweet_mode": "extended",
   "count": 100
